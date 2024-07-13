691261aa381cbf33791426825eb1c20845562e4b
suspectglint <sravanreddy.rokkam@gmail.com>
2024-07-13 17:40:03 +0530

    Creates Control_Statements.py file

---
+++ Control_Statements.py
@@ -0,0 +1,19 @@
+#Program for understanding various control statements present in python.
+
+#############################################################
+#if statement
+a=123
+
+if a == 123:
+    print("Samba is Simba") #default indentation is 4 spaces in python.
+
+a='123'
+
+if a != 123:
+ print("Simba is Samba") #But we can use any other number of spaces other than 4 as indentation.
+
+a=345
+
+if a == 345 : print(f'{a} is 345') # inline statement when we have only one statement in suite(group of statements with same indentation.)
+
+#############################################################
