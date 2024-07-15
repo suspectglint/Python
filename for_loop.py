03edcc1870763a0cf2ab88f8b36cf46dd9e05566
suspectglint <sravanreddy.rokkam@gmail.com>
2024-07-15 05:49:27 +0530

    Adds for_loop.py file.

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+#To check the usage of for loop in python
+
+a=3
+
+print(a,id(a))
+
+nums = range(10)
+print(id(nums))
+print(nums[3],id(nums[3]))
+print(nums,type(nums))
+print(nums[9],type(nums[9]))
+print(type(range(10)))
+
+#Incorrect - range() function only accepts int as an argument. no other types are allowed.
+"""charset = range(1.0,2.5,0.5)
+print(charset)"""
