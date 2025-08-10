0be76e5c5930807306e73f93071bbd1914979198
suspectglint <sravanreddy.rokkam@gmail.com>
2025-08-10 19:07:31 +0530

    Added time complexities for all the functions in heap.

--- heaps/Heaps.py
+++ heaps/Heaps.py
@@ -3,27 +3,27 @@ This python file is used to show all the functions present in heapq module with 
 """
 import heapq
 
-#Creation of Heap using heapify() function.
+#Creation of Heap using heapify() function - O(n) time complexity
 l1=[2,54,1,32,78,12,7,10]
 print(l1,type(l1))
 heapq.heapify(l1)
 print(l1,type(l1))
 
-#Popping elements using heappop() function.
+#Popping elements using heappop() function - O(logn) time complexity
 r=heapq.heappop(l1)
 print("Popped Element = ",r)
 print("Heap = ",l1)
 
-#Pushing elements using heappush() function.
+#Pushing elements using heappush() function - O(logn) time complexity
 heapq.heappush(l1,23)
 print("Heap = ",l1)
 
-#Pusing and Popping elements using heappushpop() function
+#Pusing and Popping elements using heappushpop() function - O(logn) time complexity
 r=heapq.heappushpop(l1,71)
 print("Popped Element = ",r)
 print("Heap = ",l1)
 
-#Popping and pushing elements using heapreplace() function
+#Popping and pushing elements using heapreplace() function - O(logn) time complexity
 r=heapq.heapreplace(l1,98)
 print("Popped Element = ",r)
 print("Heap = ",l1)
@@ -36,8 +36,8 @@ for i in heapq.merge([1,12],[3,7,9],[4,5],[6]):
     output.append(i)
 print("Merged Output = ",output)
 
-#Printing n smallest elements in heap using nsmallest() function
+#Printing n smallest elements in heap using nsmallest() function - O(nlogk) time complexity. k is the number of elements to be fetched.
 print("5 smallest elements in ",l1," are ",heapq.nsmallest(5,l1))
 
-#Printing n largest elements in heap using nlargest() function
+#Printing n largest elements in heap using nlargest() function - O(nlogk) time complexity. k is the number of elements to be fetched.
 print("5 largest elements in ",l1," are ",heapq.nlargest(5,l1))
